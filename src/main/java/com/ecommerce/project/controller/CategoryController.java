1fab512549b1999b0435a6c659f9b47633b9321f
nirbhay-dostiya <nirbhaykumarraj1998@gmail.com>
2026-06-03 15:52:14 +0530

    make CategoryServiceImp lightweighted

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -44,23 +44,17 @@ public class CategoryController {
 //    @DeleteMapping("/api/admin/categories/{categoryId}")
     @RequestMapping(value = "/api/admin/categories/{categoryId}", method = RequestMethod.DELETE)
     public ResponseEntity<String>  deleteCategory(@PathVariable Long categoryId){
-        try{
             String status = categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status , HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+
     }
 
     @PutMapping("/api/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category,
                                                  @PathVariable Long categoryId){
-        try{
             Category savedCategory = categoryService.updateCategory(category, categoryId);
             return new ResponseEntity<>("Category with category id: " + categoryId, HttpStatus.OK);
-        } catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
+
     }
 
 
--- src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
@@ -18,12 +18,6 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImp implements CategoryService{
 
-// List ko hata ke categoryRepository daal rahe h jo autowired se khud le lega
-//    private List<Category> categories = new ArrayList<>();
-
-//    remove this because its not need latter I will use auto generation
-//    private Long nextId = 1L;
-
     @Autowired
     private CategoryRepository categoryRepository;
 
@@ -48,14 +42,6 @@ public class CategoryServiceImp implements CategoryService{
     @Override
     public String deleteCategory(Long categoryId) {
 
-        // ye list optional ko hata ke categoryRepository user kar rahe h
-
-//        List<Category> categories = categoryRepository.findAll();
-//        Category category = categories.stream()
-//                .filter(c -> c.getCategoryId().equals(categoryId))
-//                .findFirst()
-//                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND , "Resourse not found"));
-//
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId",categoryId));
         categoryRepository.delete(category);
@@ -66,25 +52,8 @@ public class CategoryServiceImp implements CategoryService{
     @Override
     public Category updateCategory(Category category, Long categoryId) {
 
-        // ye local verial ko dismis kar diye taki categoryRepository use kar sake
 
-//   ye niche wala local variable h
 
-//        List<Category> categories = categoryRepository.findAll();
-//        Optional<Category> optionalCategory = categories.stream()
-//                .filter(c -> c.getCategoryId().equals(categoryId))
-//                .findFirst();
-//
-//        if(optionalCategory.isPresent()){
-//            Category existingCategory = optionalCategory.get();
-//            existingCategory.setCategoryName(category.getCategoryName());
-//            Category saveCategory = categoryRepository.save(existingCategory);
-//            return saveCategory;
-//        } else {
-//            throw new ResponseStatusException(HttpStatus.NOT_FOUND , "Category not found");
-//        }
-
-//    categoryRepository ka use kar rahe taki code optimal ho
         Category savedCategory = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId",categoryId));
 
